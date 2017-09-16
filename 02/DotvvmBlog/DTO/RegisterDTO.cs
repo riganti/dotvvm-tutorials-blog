700164793407462a9404ca807e0da445f38dd9d1
Tomáš Herceg <herceg@vbnet.cz>
2017-09-16 15:14:37 +0200

    Sample application for part 2 added

---
+++ 02/DotvvmBlog/DTO/RegisterDTO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotvvmBlog.DTO
+{
+    public class RegisterDTO
+    {
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+
+        [Compare(nameof(Password))]
+        public string Password2 { get; set; }
+
+        public string BlogDescription { get; set; }
+
+        public int BlogCategoryId { get; set; }
+
+        public string ThemeName { get; set; } = "Dark";
+
+        public bool AgreeWithConditions { get; set; }
+
+    }
+}
---
+++ 02/DotvvmBlog/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DotVVM.Framework.ViewModel;
+using DotvvmBlog.DTO;
+
+namespace DotvvmBlog.ViewModels
+{
+    public class RegisterViewModel : DotvvmViewModelBase
+    {
+
+        public RegisterDTO Data { get; set; } = new RegisterDTO();
+
+        public CategoryDTO[] Categories => new[]
+        {
+            new CategoryDTO() { Id = 1, Name = "Business" },
+            new CategoryDTO() { Id = 2, Name = "Culture" },
+            new CategoryDTO() { Id = 3, Name = "Design" },
+            new CategoryDTO() { Id = 4, Name = "Fashion" },
+            new CategoryDTO() { Id = 5, Name = "Lifestyle" },
+            new CategoryDTO() { Id = 6, Name = "Technology" },
+            new CategoryDTO() { Id = 7, Name = "Travel" },
+        };
+
+
+        public void Submit()
+        {
+            // will be implemented in some of the next parts
+        }
+
+    }
+}
+
